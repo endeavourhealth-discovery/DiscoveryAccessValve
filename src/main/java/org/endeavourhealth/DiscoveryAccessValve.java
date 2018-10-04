bfce89bef4713caaa4eb005ba3773bab5274b542
stuart <stuart@bearclawsolutions.co.uk>
2018-10-04 17:07:43 +0100

    Use the

--- src/main/java/org/endeavourhealth/DiscoveryAccessValve.java
+++ src/main/java/org/endeavourhealth/DiscoveryAccessValve.java
@@ -3,14 +3,12 @@ package org.endeavourhealth;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ValveBase;
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.representations.AccessToken;
+import org.endeavourhealth.usermanagermodel.models.caching.UserCache;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.security.Principal;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -36,19 +34,41 @@ public class DiscoveryAccessValve extends ValveBase {
             String authHeader = httpServletRequest.getHeader("authorization");
             logger.log(Level.INFO, "Auth Header: {0}", authHeader);
 
+            String projectId = httpServletRequest.getHeader("projectId");
+            logger.log(Level.INFO, "Project Id: {0}", projectId);
+
+            String pathURI = httpServletRequest.getRequestURI();
+            logger.log(Level.INFO, "pathURI: {0}", pathURI);
+
             //keycloak access info
-            KeycloakPrincipal kp = (KeycloakPrincipal)principal;
+            /*KeycloakPrincipal kp = (KeycloakPrincipal)principal;
             AccessToken token = kp.getKeycloakSecurityContext().getToken();
             Set<String> realmRoles = token.getRealmAccess().getRoles();
             logger.log(Level.INFO, "Keyloak -> ID: "+token.getSubject());
             logger.log(Level.INFO, "Keyloak -> Name: "+token.getGivenName()+" "+token.getFamilyName());
-            logger.log(Level.INFO, "Keyloak -> Roles: "+realmRoles);
+            logger.log(Level.INFO, "Keyloak -> Roles: "+realmRoles);*/
 
             //validate application access against the app_name param from web.xml - call into user manager model interface
             String appId = httpServletRequest.getServletContext().getInitParameter("app_name");
             logger.log(Level.INFO, "app_name -> {0}", appId);
 
-            boolean isUserAllowedAccess = appId.equalsIgnoreCase("User Manager");
+            // boolean isUserAllowedAccess = appId.equalsIgnoreCase("User Manager");
+            Boolean isUserAllowedAccess = false;
+
+            if (projectId == null && (pathURI.contains("getUserProfile") || pathURI.contains("getProjects"))) {
+                logger.log(Level.INFO, "getting profile so let it through: ");
+                isUserAllowedAccess = true;
+            } else {
+                try {
+                    logger.log(Level.INFO, "Checking in the cache for access");
+                    logger.log(Level.INFO, "UserId " + userId + "; ProjectId " + projectId + "; appName " + appId);
+                    isUserAllowedAccess = UserCache.getUserProjectApplicationAccess(userId, projectId, appId);
+
+                } catch (Exception e) {
+                    logger.log(Level.INFO, "Error: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
             if (!isUserAllowedAccess) {
 
                 logger.log(Level.INFO, "Application access not permitted -> send 403");
